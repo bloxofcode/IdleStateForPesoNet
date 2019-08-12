11ad84961d8509d0d07ee0a8d903b6a8acd09f71
unknown <bloxofcodes@gmail.com>
2019-08-12 20:42:44 +0800

    Auto Shutdown for Piso Net

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace IdleShutdown
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtSetTime = new System.Windows.Forms.TextBox();
+            this.btnSet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(64, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(64, 35);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(143, 202);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "0";
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "MouseX";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "MouseY";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 210);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Time in Seconds:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 86);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Time Set (Seconds):";
+            // 
+            // txtSetTime
+            // 
+            this.txtSetTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSetTime.Location = new System.Drawing.Point(121, 83);
+            this.txtSetTime.Name = "txtSetTime";
+            this.txtSetTime.ReadOnly = true;
+            this.txtSetTime.Size = new System.Drawing.Size(114, 20);
+            this.txtSetTime.TabIndex = 7;
+            this.txtSetTime.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnSet
+            // 
+            this.btnSet.Location = new System.Drawing.Point(175, 109);
+            this.btnSet.Name = "btnSet";
+            this.btnSet.Size = new System.Drawing.Size(60, 23);
+            this.btnSet.TabIndex = 8;
+            this.btnSet.Text = "Set";
+            this.btnSet.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(247, 232);
+            this.Controls.Add(this.btnSet);
+            this.Controls.Add(this.txtSetTime);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Idle State Detector";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtSetTime;
+        private System.Windows.Forms.Button btnSet;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IdleShutdown
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        static extern bool GetCursorPos(ref Point lpPoint);
+
+        public string prevX = string.Empty;
+        public string prevY = string.Empty;
+
+
+        private int counter = 0;
+
+
+        private Form frm;
+
+        private int timeInSeconds; 
+
+
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            
+            timer1.Start();
+            timer2.Start();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //TODO : Check if file is Existing
+            //  If not force user to create it
+
+
+
+
+            timeInSeconds = 0;
+            frm = new Form2();
+            string textFile = AppDomain.CurrentDomain.BaseDirectory + @"Config.dump";
+            string [] lines = File.ReadAllLines(textFile);
+
+            timeInSeconds = Convert.ToInt32(lines[0].Split('=')[1]);
+
+
+            txtSetTime.Text = timeInSeconds.ToString();
+
+
+            this.WindowState = FormWindowState.Minimized;
+            //foreach (string line in lines)
+            //    Console.WriteLine(line);
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Point pt = new Point();
+
+            GetCursorPos(ref pt);
+            textBox1.Text = pt.X.ToString();
+            textBox2.Text = pt.Y.ToString();
+
+
+
+            //https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-mouse_event
+            //https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getcursorpos
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            label1.Text = counter.ToString();
+
+            if (counter == timeInSeconds - 30)
+            {
+                frm.TopMost = true;
+                frm.StartPosition = FormStartPosition.CenterScreen;
+                frm.Show();
+            }
+
+
+            if (textBox1.Text != prevX)
+            {
+                prevX = textBox1.Text;
+                counter = 0;
+                frm.Hide();
+                return;
+            }
+
+
+            counter = counter + 1;
+
+
+
+
+
+
+            if (counter == timeInSeconds + 60)
+            {
+
+                var psi = new ProcessStartInfo("shutdown", "/s /t 0");
+                psi.CreateNoWindow = true;
+                psi.UseShellExecute = false;
+                Process.Start(psi);
+
+
+
+                foreach (Process p in Process.GetProcesses(System.Environment.MachineName))
+
+                {
+
+                    if (p.MainWindowHandle != IntPtr.Zero)
+
+                    {
+
+                        p.Kill();
+
+
+
+                    }
+
+                }
+            }
+            
+
+
+
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            bool mousePointerNotOnTaskBar = Screen.GetWorkingArea(this).Contains(Cursor.Position);
+            if(this.WindowState == FormWindowState.Minimized && mousePointerNotOnTaskBar)
+            {
+                notifyIcon1.Icon = new Icon(AppDomain.CurrentDomain.BaseDirectory + @"mouse.ico");
+                notifyIcon1.BalloonTipText = "Pisonet Idle State Detector";
+                notifyIcon1.ShowBalloonTip(500);
+                this.ShowInTaskbar = false;
+                notifyIcon1.Visible = true;
+            }
+
+
+
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+
+            if(this.WindowState == FormWindowState.Normal)
+            {
+                this.ShowInTaskbar = true;
+                notifyIcon1.Visible = false;
+            }
+        }
+    }
+}
